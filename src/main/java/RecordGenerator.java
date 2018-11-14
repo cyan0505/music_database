1d80be01821d9b09fca94eaf9f96c735dc76c449
Cyan <dawid.grygier5@gmail.com>
2018-11-14 11:01:28 +0100

    code clean

--- src/main/java/RecordGenerator.java
+++ src/main/java/RecordGenerator.java
@@ -1,9 +1,7 @@
-package main.java;
-
-
 import java.io.FileNotFoundException;
 
 public class RecordGenerator {
+
     public static void main(String[] args) throws FileNotFoundException {
 
         ArtistGenerator artistGenerator = new ArtistGenerator();
